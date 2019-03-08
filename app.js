607983eef37719646d697cfbe045ac67f870cb4f
Vaerilius <tjohannest@gmail.com>
2019-03-08 20:47:15 +0200

    update

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-const UI = (function () {
-   let text = 'private text';
-   const thisIsPrivateFunction = function () {
-      const h1 = document.querySelector('h1');
-      h1.textContent = text;
+//Standard module pattern
+
+// const UI = (function () {
+//    let text = 'private text';
+//    const thisIsPrivateFunction = function () {
+//       const h1 = document.querySelector('h1');
+//       h1.textContent = text;
+//    }
+//    return {
+//       thisIsPublicFunction: function() {
+//          thisIsPrivateFunction(text);
+//          console.log(text);
+//       }
+//    }
+// })();
+
+// UI.thisIsPublicFunction();
+
+
+//revealing module pattern
+
+const ItemCtrl = (function () {
+   const array = [];
+
+   function add(object) {
+      array.push(object);
+      console.log('object added');
+   }
+   function get(id) {
+      return array.find(find => {
+         return find.id=== id;
+      });
    }
    return {
-      thisIsPublicFunction: function() {
-         thisIsPrivateFunction(text);
-         console.log(text);
-      }
+      add: add,
+      get: get
    }
 })();
 
-UI.thisIsPublicFunction();
+ItemCtrl.add({id: 1, name: 'John'});
+console.log(ItemCtrl.get(1));
